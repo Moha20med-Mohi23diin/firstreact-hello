36b7351f289ad1461d1ba89b4389ce997da7f2b2
Mohamed Mohidiin <91695293+Moha20med-Mohi23diin@users.noreply.github.com>
2022-03-07 09:43:48 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello React , I'm Mohamed Mohidiin 
+        <h1> On Journey to Full-Stack Devoloper</h1>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
